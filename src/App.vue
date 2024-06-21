00b932a339a97ac81227edb2ff90f11c0179adc7
krios2146 <krios2146@gmail.com>
2024-06-21 14:42:01 +0700

    feat: add auth tests to the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,43 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { retrieveLaunchParams, type LaunchParams } from '@tma.js/sdk'
+import { ref } from 'vue'
+import axios, { AxiosError } from 'axios'
+
+const launchParams = ref<LaunchParams | undefined>()
+const launchParamsError = ref()
+const response = ref()
+const error = ref()
+
+try {
+  launchParams.value = retrieveLaunchParams()
+} catch (error) {
+  launchParamsError.value = error
+}
+
+const url = import.meta.env.VITE_BACKEND_URL
+
+axios
+  .post(url, launchParams.value?.initDataRaw)
+  .then((apiResponse) => (response.value = apiResponse))
+  .catch((apiError: AxiosError) => (error.value = apiError))
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <h1>Init params</h1>
+    <pre>{{ launchParams?.initDataRaw }}</pre>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div v-if="launchParamsError">
+      <h1>Init params error</h1>
+      <pre>{{ launchParamsError }}</pre>
+    </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <h1>API Response</h1>
+    <div>{{ response }}</div>
+
+    <div v-if="error">
+      <h1>API Error</h1>
+      <pre>{{ JSON.stringify(error, null, 2) }}</pre>
     </div>
   </header>
-
-  <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
