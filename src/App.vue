ff24e3ae2f13e4518ce8c38f1c35f70b1865f5c6
Vladimir Kidyaev <krios2146@gmail.com>
2025-04-04 14:58:30 +0700

    refactor(app): remove api request

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <script setup lang="ts">
 import { retrieveLaunchParams, type LaunchParams } from '@tma.js/sdk'
 import { ref } from 'vue'
-import axios, { AxiosError, type AxiosResponse } from 'axios'
 
 const launchParams = ref<LaunchParams | undefined>()
 const launchParamsError = ref()
-const response = ref<AxiosResponse>()
-const error = ref()
 
 try {
   launchParams.value = retrieveLaunchParams()
 } catch (error) {
   launchParamsError.value = error
 }
-
-const url = import.meta.env.VITE_BACKEND_URL
-
-axios
-  .post(`${url}/auth/sign-in/by-telegram`, {
-    initDataRaw: launchParams.value?.initDataRaw,
-    initData: launchParams.value?.initData
-  })
-  .then((apiResponse) => (response.value = apiResponse))
-  .catch((apiError: AxiosError) => (error.value = apiError))
 </script>
 
 <template>
@@ -33,16 +20,6 @@ axios
     <div v-if="launchParamsError">
       <h1>Init params error</h1>
       <pre>{{ launchParamsError }}</pre>
-    </div>
-
-    <h1>API Response</h1>
-    <pre>{{ JSON.stringify(response, null, 2) }}</pre>
-
-    <div v-if="response?.status === 200">Telegram auth is successful</div>
-
-    <div v-if="error">
-      <h1>API Error</h1>
-      <pre>{{ JSON.stringify(error, null, 2) }}</pre>
     </div>
   </header>
 </template>
